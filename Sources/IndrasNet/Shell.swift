57f8c7332f186f23a99851cc7204593256dbeaf9
zane <39070793+zaneenders@users.noreply.github.com>
2026-06-07 23:01:40 -0600

    drop internal job system

--- Sources/IndrasNet/Shell.swift
+++ Sources/IndrasNet/Shell.swift
@@ -12,7 +12,7 @@ extension Shell {
       if instance.role == .leader {
         let heartbeat = AppendEntries.Args(term: instance.currentTerm, leaderId: instance.id)
         for peer in instance.peers {
-          sendAppendEntries(to: peer, args: heartbeat)
+          await deliverAppendEntries(to: peer, args: heartbeat)
         }
         try? await Task.sleep(for: .milliseconds(500))
         continue
@@ -33,7 +33,7 @@ extension Shell {
       for action in instance.onElectionTimeOut() {
         switch action {
         case .requstVote(let peer, let args):
-          sendRequestVote(to: peer, args: args)
+          await deliverRequestVote(to: peer, args: args)
         }
       }
       resetElectionTimeout()
@@ -46,18 +46,6 @@ extension Shell {
 
   private func randomElectionTimeout() -> Duration {
     Duration(.milliseconds(Int64.random(in: 1500..<3000)))
-  }
-
-  private func sendRequestVote(to peer: PeerId, args: RequestVote.Args) {
-    self.enqueue { await self.deliverRequestVote(to: peer, args: args) }
-  }
-
-  private func sendRequestVoteReply(to peer: PeerId, term: Term, voteGranted: Bool) {
-    self.enqueue { await self.deliverRequestVoteReply(to: peer, term: term, voteGranted: voteGranted) }
-  }
-
-  private func sendAppendEntries(to peer: PeerId, args: AppendEntries.Args) {
-    self.enqueue { await self.deliverAppendEntries(to: peer, args: args) }
   }
 
   private func deliverRequestVote(to peer: PeerId, args: RequestVote.Args) async {
@@ -76,25 +64,25 @@ extension Shell {
     await deliver(to: peer, message: .appendEntries(args), kind: "appendEntries")
   }
 
-  func receiveMessage(message: AppMessage, from peer: PeerId) {
+  func receiveMessage(message: AppMessage, from peer: PeerId) async {
     switch message {
     case .requestVote(let args):
       logEvent(kind: "requestVote", direction: "in", peer: peer)
-      onRequestVote(from: peer, request: args)
+      await onRequestVote(from: peer, request: args)
     case .requestVoteReply(let reply):
       logEvent(kind: "requestVoteResponse", direction: "in", peer: peer)
-      onRequestVoteResponse(from: peer, reply: reply)
+      await onRequestVoteResponse(from: peer, reply: reply)
     case .appendEntries(let args):
       logEvent(kind: "appendEntries", direction: "in", peer: peer)
       onAppendEntries(from: peer, args: args)
     }
   }
 
-  private func onRequestVote(from peer: PeerId, request: RequestVote.Args) {
+  private func onRequestVote(from peer: PeerId, request: RequestVote.Args) async {
     for action in instance.onRequestVote(peer, request) {
       switch action {
       case .sendRequestVoteReply(let to, let term, let voteGranted):
-        sendRequestVoteReply(to: to, term: term, voteGranted: voteGranted)
+        await deliverRequestVoteReply(to: to, term: term, voteGranted: voteGranted)
       case .roleChanged, .persist:
         ()
       case .resetElectionTimeout:
@@ -103,11 +91,11 @@ extension Shell {
     }
   }
 
-  private func onRequestVoteResponse(from peer: PeerId, reply: RequestVote.Reply) {
+  private func onRequestVoteResponse(from peer: PeerId, reply: RequestVote.Reply) async {
     for action in instance.onRequestVoteReply(peer, reply) {
       switch action {
       case .sendAppendEntry(let peer, let args):
-        sendAppendEntries(to: peer, args: args)
+        await deliverAppendEntries(to: peer, args: args)
       }
     }
   }
@@ -132,9 +120,7 @@ public actor Shell {
   let transport: TCPTransport
   private let logger: Logger
   private var endpoints: [PeerId: NodeAddress] = [:]
-  private typealias Job = @Sendable () async -> Void
-  private var supervisor: Task<Void, Never>?
-  private var cancelableJobs: AsyncStream<Job>.Continuation?
+  private var electionTimer: Task<Void, Never>?
 
   public init(_ node: NodeAddress, transport: TCPTransport, logger: Logger? = nil) {
     self.peerId = node.addressKey
@@ -148,29 +134,16 @@ public actor Shell {
   }
 
   public func start(with peers: [NodeAddress]) async throws -> Int {
-    if self.supervisor != nil {
-      guard let port = await transport.listenPort() else { throw ShellError.noListenPort }
-      return port
-    }
-
     self.endpoints = Dictionary(uniqueKeysWithValues: peers.map { ($0.addressKey, $0) })
     self.instance = Instance(id: peerId, peers: Set(self.endpoints.keys))
-
-    let (stream, continuation) = AsyncStream.makeStream(of: Job.self)
-    self.cancelableJobs = continuation
-    self.supervisor = Task {
-      await withDiscardingTaskGroup { group in
-        for await job in stream {
-          group.addTask { await job() }
-        }
-      }
-    }
 
     try await transport.start { message, from in
       await self.receiveMessage(message: message, from: from)
     }
 
-    self.enqueue { await self.startElectionTimer() }
+    self.electionTimer = Task {
+      await self.startElectionTimer()
+    }
 
     guard let port = await transport.listenPort() else {
       await stop()
@@ -204,15 +177,9 @@ public actor Shell {
   }
 
   public func stop() async {
-    self.cancelableJobs?.finish()
-    self.cancelableJobs = nil
-    self.supervisor?.cancel()
-    _ = await self.supervisor?.value
-    self.supervisor = nil
-  }
-
-  private func enqueue(_ job: @escaping Job) {
-    self.cancelableJobs?.yield(job)
+    self.electionTimer?.cancel()
+    _ = await self.electionTimer?.value
+    self.electionTimer = nil
   }
 
   private func ensureConnected(to peer: PeerId) async -> Bool {
